252c4dfd7a13fea941e6bfa5b2df9bafd053e951
damodara <pendeladamodhara@gmail.com>
2019-06-24 22:42:34 +0530

    intial commit

---
+++ HytrixClientExample/src/main/java/com/damu/HytrixClientExampleApplication.java
@@ -0,0 +1,27 @@
+package com.damu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@RestController
+@EnableZuulProxy
+public class HytrixClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HytrixClientExampleApplication.class, args);
+	}
+
+	@GetMapping("/client")
+	public String getMessage() {
+		String url = "http://localhost:2018/getMessage";
+		RestTemplate template = new RestTemplate();
+		return template.getForEntity(url, String.class).getBody();
+
+	}
+
+}
